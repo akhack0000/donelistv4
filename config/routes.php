6a987aa7d2bddedc90875d558fd1da08705b93b6
akhack0000 <gixarakusi@gmail.com>
2025-11-02 02:57:53 +0900

    [ai] CakePHPデフォルトのPagesコントローラーとテンプレートを削除
    - PagesControllerを削除
    - templates/Pagesディレクトリを削除
    - PagesControllerTestを削除
    - ルートパス(/)をHomeコントローラーに変更
    - サービスに関係ないデフォルトページを完全削除

--- config/routes.php
+++ config/routes.php
@@ -51,21 +51,10 @@ return function (RouteBuilder $routes): void {
 
     $routes->scope('/', function (RouteBuilder $builder): void {
         /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
+         * DonelistV4 routes
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
-        /*
-         * /home endpoint for DonelistV4 title page
-         */
+        $builder->connect('/', ['controller' => 'Home', 'action' => 'index']);
         $builder->connect('/home', ['controller' => 'Home', 'action' => 'index']);
-
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
-        $builder->connect('/pages/*', 'Pages::display');
 
         /*
          * Connect catchall routes for all controllers.
